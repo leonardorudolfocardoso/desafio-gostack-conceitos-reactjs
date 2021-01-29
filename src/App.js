cea28150ee32b1037d360b5851d21b12f2781c67
Leonardo Rudolfo Cardoso <leonardorudolfocardoso@gmail.com>
2021-01-29 16:15:35 -0300

    Challenge finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {v4 as uuid} from "uuid";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get("repositories").then(response => {
+        setRepositories(response.data)
+      }
+    )}, []);
+
   async function handleAddRepository() {
     // TODO
+    const newRepository = {
+      // id: uuid(), 
+      id: "123",
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    };
+
+    const response = await api.post(
+      "/repositories", 
+      newRepository
+    );
+    
+    if (response.status===200) {
+      // console.log(`Added repository ${JSON.stringify(newRepository)}`)
+      setRepositories([...repositories, response.data]);
+    } 
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    if (response.status===204) {
+      // console.log(`Removed repository with ID ${id}`);
+      setRepositories(repositories.filter(repository => repository.id !== id));
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
